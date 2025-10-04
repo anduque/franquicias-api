871d8685d1ebd34f757933bd97f28bd0df643470
anduque <anduque2000@gmail.com>
2025-10-03 21:10:33 -0500

    Add Sucursal.java entity model

---
+++ src/main/java/com/anduque/franquiciasapi/model/Sucursal.java
@@ -0,0 +1,22 @@
+package com.anduque.franquiciasapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sucursal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+}
