b0b4c75153dac6671837a576e4942d8f8ccc1381
anduque <anduque2000@gmail.com>
2025-10-12 19:05:52 -0500

    Clear dependencies

--- src/main/java/com/anduque/franquiciasapi/model/Sucursal.java
+++ src/main/java/com/anduque/franquiciasapi/model/Sucursal.java
@@ -1,14 +1,22 @@
 package com.anduque.franquiciasapi.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
